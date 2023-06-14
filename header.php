e2c6df6978cf614f551eeac6d52cc3f0543de9bc
AlbertCodeur <128040628+AlbertCodeur@users.noreply.github.com>
2023-06-14 16:27:15 +0000

    les choses serieuses commences!

--- header.php
+++ header.php
@@ -1,15 +1,23 @@
+<?php
+session_start();
+$_SESSION['id']=1;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <script src="https://kit.fontawesome.com/6c773daca8.js" crossorigin="anonymous"></script>
+    <script defer src="include/fontawesome/js/all.min.js"></script>
+    <link href="include/fontawesome/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="include/bulma/css/bulma.min.css">
+    <link rel="stylesheet" href="style.css">
     <title>ChatRooster</title>
 </head>
 <body>
-<nav class="navbar is-light" role="navigation" aria-label="main navigation">
+<nav class="navbar is-light is-fixed-top" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="">
       <img src="include/img/poulet1.png" alt="logo" width="30" height="50" style="border-radius: 50%; margin: 2px; border: solid red 1px;"> ChatRooster
@@ -63,9 +71,16 @@
           <a class="button is-primary">
             <strong>Sign up</strong>
           </a>
+          <?php
+          if($_SESSION['id']==null){ ?>
+          <a class="button is-light">
+            Log out
+          </a>
+          <?php }else {  ?>
           <a class="button is-light">
             Log in
           </a>
+          <?php } ?>
         </div>
       </div>
     </div>
@@ -94,5 +109,6 @@ $navbarBurgers.forEach( el => {
 
 });
 </script>
-</body>
-</html>
+<?php
+include "footer.php";
+?>
--- index.php
+++ index.php
@@ -1,3 +1,65 @@
 <?php
 include "header.php";
 ?>
+<div class="columns is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd" style="width:auto; height: 100% ; margin-top: 5%;">
+<div style="width: 30%;position: fixed;" class="column  ">
+<aside class="menu">
+  <p class="menu-label">
+    General
+  </p>
+  <ul class="menu-list">
+    <li><a>Dashboard</a></li>
+    <li><a>Customers</a></li>
+  </ul>
+  <p class="menu-label">
+    Administration
+  </p>
+  <ul class="menu-list">
+    <li><a>Team Settings</a></li>
+    <li>
+      <a class="is-active">Manage Your Team</a>
+      <ul>
+        <li><a>Members</a></li>
+        <li><a>Plugins</a></li>
+        <li><a>Add a member</a></li>
+      </ul>
+    </li>
+    <li><a>Invitations</a></li>
+    <li><a>Cloud Storage Environment Settings</a></li>
+    <li><a>Authentication</a></li>
+  </ul>
+  <p class="menu-label">
+    Transactions
+  </p>
+  <ul class="menu-list">
+    <li><a>Payments</a></li>
+    <li><a>Transfers</a></li>
+    <li><a>Balance</a></li>
+  </ul>
+</aside>
+</div>
+<div style="width: 50%;margin: 20px; margin-left:40% ; margin-bottom: 15px; height: auto; position: absolute;" class="column">
+<figure class="image is-16by9">
+  <iframe class="has-ratio" width="640" height="360" src="https://www.youtube.com/embed/YE7VzlLtp-4" frameborder="0" allowfullscreen></iframe>
+</figure>
+<div style="margin: 10px;">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt laudantium unde ipsa expedita et. Laboriosam enim corporis omnis dolores saepe ullam necessitatibus, iusto doloribus perferendis nisi animi quos accusamus est debitis distinctio facilis ut velit illo. Est, facilis dicta cupiditate, unde delectus iure nobis placeat neque, dolores fuga officia. Voluptatem.</p>
+</div>
+<div style=" padding: 10px; width: 100%; position: relative; display: inline-flex; height: 10%;">
+<div class="field has-addons" style=" width: 100%; right: 50px; margin: 20px;padding-right:0px;">
+<div>
+<input class=" control input is-primary" type="text" placeholder="commentaires">
+</div>
+<div class=" control">
+<a class="button is-primary" type="submit">envoyer</a>
+</div>
+</div>
+<div style=" margin-top: 25px; text-align: right; margin-right: 10px; font-size: 20px; width: 20px; height: 20px; color: lightgray;">
+<a href=""><i class="fa fa-thumbs-up" aria-hidden="true"></i></a>
+</div>
+<div  style=" margin-top: 25px; text-align: right; font-size: 20px; color: lightgray ; width: 20px; height: 20px;">
+<a href=""><i class="fa fa-share" aria-hidden="true"></i></a>
+</div>
+</div>
+</div>
+</div>
